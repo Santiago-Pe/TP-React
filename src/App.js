eca59c0fc347fc292bbde9ee9637b4d4f14b9898
Santiago Peña <santiago.lae97@gmail.com>
2021-10-11 15:34:24 -0300

    NavBar con Css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import NavBar from "./components/NavBar";
+import './components/NavBar.css';
 
 function App() {
+  const dropDowNav = ()=>
+  {
+      let dropDowList = document.getElementsByClassName("dropDow");
+      dropDowList.onClick("click", ()=>
+      {
+          
+      });
+  }  
   return (
     <div className="Nav">
     <NavBar/>   
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,22 +1,23 @@
 const NavBar = () => 
 {
     return (
-        <nav>
-            <ul className = "navMain">
-                <li><a href="">Inicio</a></li>
-                <li><a href="">Bebidas</a>
-                    <ul>
-                        <li><a href="">Vinos</a></li>
-                        <li><a href="">Whiskys</a></li>
-                        <li><a href="">Licores</a></li>
-                        <li><a href="">Vermut</a></li>
-                        <li><a href="">Vodkas</a></li>
-                    </ul>  
-                </li>
-                <li><a href="">Contacto</a></li>
-            </ul>
-      </nav>
-
+        <header>
+            <nav className = "navMain">
+                <ul className = "navList">
+                    <li><a href="">Inicio</a></li>
+                    <li><a className="dropDown" href="">Bebidas</a>
+                        <ul className="dropDowList">
+                            <li><a href="">Vinos</a></li>
+                            <li><a href="">Whiskys</a></li>
+                            <li><a href="">Licores</a></li>
+                            <li><a href="">Vermut</a></li>
+                            <li><a href="">Vodkas</a></li>
+                        </ul>  
+                    </li>
+                    <li><a href="">Contacto</a></li>
+                </ul>
+        </nav>
+    </header>
     )
     
 }
