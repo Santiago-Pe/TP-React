f72a4ede12caa269e410335a6c3e761c50678112
Santiago Peña <santiago.lae97@gmail.com>
2021-10-13 17:30:15 -0300

    Afetter Class 2 - NavBar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+// IMPORTS- Requiero librerias, componentes o lo que necesite.
+// COMPONENT - Desarrollo lo que haga mi componente
+//EXPORT - Exportar mi componente para poder utilizarlo en otras partes del codigo.
+import { Navbar, Container, Nav} from 'react-bootstrap';
+const NavBar = () =>
+{
+    //Logic Develop
+    return(
+        <>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                <Nav.Link href="#home">Home</Nav.Link>
+                <Nav.Link href="#features">Features</Nav.Link>
+                <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+                </Container>
+            </Navbar>
+        </>
+    )
+}
+
+export default NavBar
