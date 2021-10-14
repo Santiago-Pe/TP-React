0dffaa5fd0382b5d585c8fcdf9af9ddf832d3332
Santiago Peña <santiago.lae97@gmail.com>
2021-10-14 14:32:59 -0300

    NavBar CSS + Title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import NavBar from "./components/NavBar";
-import './components/NavBar.css';
+import NavBar from "./components/index/NavBar";
+import './components/index/NavBar.css';
 
 function App() 
 {
--- src/components/index/NavBar.jsx
+++ src/components/index/NavBar.jsx
@@ -1,14 +1,20 @@
+export const TituloH1 = ({nombreTitulo}) =>
+{
+    return(
+        <h1 className= "title">{nombreTitulo}</h1>
+    )
+}
 
 const NavBar = () => 
 {
     return (
     // Cambiar los links de los "a" cuando esten las paginas creadas
         <header>
-            <nav>
-                <ul>
+            <nav className="navbar">
+                <ul className = "navbarMain">
                     <li><a href="">Inicio</a></li>
                     <li><a href="">Productos</a>
-                        <ul>
+                        <ul className = "subNavbarMain">
                             <li><a href="">Vinos</a></li>
                             <li><a href="">Vermuts</a></li>
                             <li><a href="">Vodkas</a></li>
@@ -21,6 +27,7 @@ const NavBar = () =>
                     <li><a href="">Quienes somos</a></li>
                 </ul>
             </nav>
+            <TituloH1 nombreTitulo="Club Cup"/>
         </header>
     )
     
