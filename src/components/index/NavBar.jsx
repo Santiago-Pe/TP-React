4a24a0bdb10d75346d88dcd9b456bf0c5e3dd164
Santiago Peña <santiago.lae97@gmail.com>
2021-10-16 21:14:51 -0300

    CartWidget Posicionado con flexbox

--- src/components/index/NavBar.jsx
+++ src/components/index/NavBar.jsx
@@ -12,25 +12,27 @@ const NavBar = () =>
     // Cambiar los links de los "a" cuando esten las paginas creadas
         <header>
             <nav className="navbar">
-                <ul className = "navbarMain">
-                    <li><a href="">Inicio</a></li>
-                    <li><a href="">Productos</a>
-                        <ul className = "subNavbarMain">
-                            <li><a href="">Vinos</a></li>
-                            <li><a href="">Vermuts</a></li>
-                            <li><a href="">Vodkas</a></li>
-                            <li><a href="">Gines</a></li>
-                            <li><a href="">Licores</a></li>
-                            <li><a href="">Whiskys</a></li>
-                            <li><a href="">Champagne</a></li>
-                        </ul>
-                    </li>    
-                    <li><a href="">Contacto</a></li>
-                    <li><a href="">Quienes somos</a></li>
+                <ul className = "boxNavbar">
+                    <div className = "navbarMain" >
+                        <li><a href="">Inicio</a></li>
+                        <li><a href="">Productos</a>
+                            <ul className = "subNavbarMain">
+                                <li><a href="">Vinos</a></li>
+                                <li><a href="">Vermuts</a></li>
+                                <li><a href="">Vodkas</a></li>
+                                <li><a href="">Gines</a></li>
+                                <li><a href="">Licores</a></li>
+                                <li><a href="">Whiskys</a></li>
+                                <li><a href="">Champagne</a></li>
+                            </ul>
+                        </li>    
+                        <li><a href="">Contacto</a></li>
+                        <li><a href="">Quienes somos</a></li>
+                    </div>    
                     <div className="boxCartWidget">    
-                        <li><a href="">Carrito</a>
+                        <a href="">
                             <CartWidget/>
-                        </li>
+                        </a>        
                     </div>    
                 </ul>
             </nav>
