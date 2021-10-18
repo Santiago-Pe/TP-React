049704d763072e48ff541410ccb012f55e3a24a6
Santiago Peña <santiago.lae97@gmail.com>
2021-10-18 10:49:02 -0300

    recupero final

---
+++ src/componentes/CartWidget.js
@@ -0,0 +1,10 @@
+import { BsFillCartFill } from "react-icons/bs";
+
+const CartWidget = () =>
+{
+    return(
+        <BsFillCartFill className = "cartWidget"/>
+    )
+}
+
+export default CartWidget;
---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,45 @@
+import CartWidget from "./CartWidget"
+export const TituloH1 = ({nombreTitulo}) =>
+{
+    return(
+        <h1 className= "title">{nombreTitulo}</h1>
+    )
+}
+
+const NavBar = () => 
+{
+    return (
+    // Cambiar los links de los "a" cuando esten las paginas creadas
+        <header>
+            <nav className="navbar">
+                <ul className = "boxNavbar">
+                    <div className = "navbarMain" >
+                        <li><a href="">Inicio</a></li>
+                        <li><a href="">Productos</a>
+                            <ul className = "subNavbarMain">
+                                <li><a href="">Vinos</a></li>
+                                <li><a href="">Vermuts</a></li>
+                                <li><a href="">Vodkas</a></li>
+                                <li><a href="">Gines</a></li>
+                                <li><a href="">Licores</a></li>
+                                <li><a href="">Whiskys</a></li>
+                                <li><a href="">Champagne</a></li>
+                            </ul>
+                        </li>    
+                        <li><a href="">Contacto</a></li>
+                        <li><a href="">Quienes somos</a></li>
+                    </div>    
+                    <div className="boxCartWidget">    
+                        <a href="">
+                            <CartWidget/>
+                        </a>        
+                    </div>    
+                </ul>
+            </nav>
+            <TituloH1 nombreTitulo="Club Cup"/>
+        </header>
+    )
+    
+}
+
+export default NavBar 
