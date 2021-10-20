0f60d97dee93df50280716698feb6b3a4cfeb63d
Santiago Peña <santiago.lae97@gmail.com>
2021-10-20 12:05:02 -0300

    ButtonCartWidget por separado

---
+++ src/components/Buttons/ButtonAdd.js
@@ -0,0 +1,16 @@
+import GrFormAdd from "./GrFormAdd"
+const ButtonAdd = () =>
+{
+    const sumar = ()=>
+    {
+        
+    }
+    return(
+        
+        <button class="buttonAdd" onClick ={sumar}>
+            <GrFormAdd/>
+        </button>
+
+    )
+}
+export default ButtonAdd
---
+++ src/components/Buttons/ButtonRemove.js
@@ -0,0 +1,13 @@
+import IoIosRemove from "./IoIosRemove"
+const ButtonRemove = () =>
+{
+
+    return(
+        
+        <button class="buttonAdd" onClick ={restar}>
+            <IoIosRemove/>
+        </button>
+
+    )
+}
+export default ButtonRemove
