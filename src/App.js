cced80c6e148f8637bd87ae6b07ac96d41f3c17c
Santiago Peña <santiago.lae97@gmail.com>
2021-10-20 23:21:34 -0300

    Promise (sin correccion de tarea)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import NavBar from "./components/NavBar/NavBar";
 import ButtonCartWidget from "./components/Buttons/ButtonCartWidget";
+// import GetFetch from "./components/p romises/Promise";
+
 // import ButtonCartWidget from "./components/Buttons/ButtonCartWidget"
 
 function App() 
@@ -8,7 +10,7 @@ function App()
     <div className="Nav">
     <NavBar/>  
     <ButtonCartWidget/>
-
+    {/* <GetFetch/> */}
     </div>
   );
 }
