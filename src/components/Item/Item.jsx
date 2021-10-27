9a85c8b8eabcf1a02822c434c1ac03ab11505632
Santiago Peña <santiago.lae97@gmail.com>
2021-10-27 15:23:59 -0300

    Renombre y separacion de componentes

---
+++ src/components/Item/Item.jsx
@@ -0,0 +1,21 @@
+import ButtonGeneral from "../Buttons/ButtonGeneral";
+const Item = ({prod}) =>
+{
+    return(
+        <div className="containerList">
+            <div>
+                <img src={prod.img} alt="Imagenes de bebidas" className="imgCard"/>
+            </div>
+            <div className="cardFooter">
+                <h4 className="titleH4">{prod.class}</h4>
+                <hr />
+                {prod.name}
+            </div>
+            <div className="buttonCard">
+                <ButtonGeneral className="btnGeneral" text="Ver Detalle"/>
+            </div>
+        </div>
+    )
+}
+
+export default Item
