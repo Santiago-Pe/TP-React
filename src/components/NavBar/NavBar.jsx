eecb86fc7f0e0127581deefb9d88014344258dd7
Santiago Peña <santiago.lae97@gmail.com>
2021-11-28 22:35:28 -0300

    NavBar css + mobile

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,12 +9,51 @@ const NavBar = () =>
 {
     const {cantProdAcum} = useCartContext()
     const cantAcum = cantProdAcum()
+
+    //Codigo para mobile
     
     return (
-    // Cambiar los links de los "a" cuando esten las paginas creadas
         <header>
-            
-            <nav className="navbar">
+
+            <span class="nvb" id="btnMenu"><i class="fas fa-bars"></i> Menu</span>
+            <div className="boxLogo">    
+                    <Link to={`/`}>
+                        <img src={"./assets/img/logoBlanco.png"} alt="ImagenLogo" className="logo" />
+                    </Link>       
+            </div> 
+
+            <nav>
+
+            </nav>
+            <ul className = "boxNavbar">   
+                    <div className = "navbarMain" >
+                        <li><Link to={`/`}>Inicio</Link></li>  
+                        <li><Link to={`/`}>Productos</Link>
+                            <ul className = "subNavbarMain">
+                                <div>
+                                    <li><Link to={`/categoria/Vinos`}>Vinos</Link></li>
+                                    <li><Link to={`/categoria/Vermuts`}>Vermuts</Link></li>
+                                    <li><Link to={`/categoria/Vodkas`}>Vodkas</Link></li>
+                                    <li><Link to={`/categoria/Gins`}>Gins</Link></li>
+                                </div>
+                                <div>
+                                    <li><Link to={`/categoria/Licores`}>Licores</Link></li>
+                                    <li><Link to={`/categoria/Whiskys`}>Whiskys</Link></li>
+                                    <li><Link to={`/categoria/Champagnes`}>Champagne</Link></li>
+                                </div>    
+                            </ul>
+                        </li>    
+                        <li><Link to={`/`}>Contacto</Link></li>
+                        <li><Link to={`/`}>Quienes somos</Link></li>
+                    </div>
+                </ul>
+                <div className="boxCartWidget">
+                    <Link to={`/cart`}> 
+                    <label><CartWidget className="cartWidget"/></label>
+                    <label>{cantAcum}</label>
+                    </Link>
+                </div>  
+            {/* <nav className="navbar">
                 <div className="boxLogo">    
                     <Link to={`/`}>
                         <img src={"./assets/img/logoBlanco.png"} alt="ImagenLogo" className="logo" />
@@ -48,7 +87,7 @@ const NavBar = () =>
                     <label>{cantAcum}</label>
                     </Link>
                 </div>  
-            </nav>
+            </nav> */}
         </header>
     )
     
